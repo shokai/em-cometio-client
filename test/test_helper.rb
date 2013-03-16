23d834ccd3f3f476e1a9c294abea45f412eed8e2
Sho Hashimoto <hashimoto@shokai.org>
2013-03-17 05:27:09 +0900

    fix test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
+require 'bundler/setup'
+$:.unshift File.expand_path '../lib', File.dirname(__FILE__)
 require 'minitest/autorun'
 require 'em-cometio-client'
 require File.expand_path 'app', File.dirname(__FILE__)
-
-$:.unshift File.expand_path '../lib', File.dirname(__FILE__)
 
 ['SIGHUP', 'SIGINT', 'SIGKILL', 'SIGTERM'].each do |sig|
   Kernel.trap sig do
